ed60ee0f2c0622a255976b7f02f7412b22578fb6
leventeszamosi <levente@alpaca.markets>
2025-08-19 13:13:06 +0200

    PLTF-1888: origin drop fix part 3

--- streamer/kuvasz-streamer.go
+++ streamer/kuvasz-streamer.go
@@ -80,39 +80,40 @@ func main() {
 	_ = lim.Wait(context.Background()) // REMOVE ME
 	// Start main loop
 	RootChannel = make(chan string)
-	for {
-		SetStatus(StatusStarting)
-		err := SetupDestination()
-		if err != nil {
-			log.Error("Error setting up destination", "err", err)
-			os.Exit(1)
-		}
-		ReadMap()
-		dbmap.CompileRegexes()
-		// Create root context allowing cancellation of all goroutines
-		rootContext, rootCancel := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
 
-		// Loop through config and replicate databases
-		log.Info("Start processing source databases")
-		for _, database := range dbmap {
-			for i, url := range database.Urls {
-				log.Info("Starting replication thread", "db-sid", database.Name+"-"+url.SID, "url", url.URL)
-				wg.Add(1)
-				go DoReplicateDatabase(rootContext, database, &database.Urls[i])
-			}
-		}
-		SetStatus(StatusActive)
-
-		select {
-		case <-RootChannel:
-			rootCancel()
-		case <-rootContext.Done():
-		}
-		SetStatus(StatusStopping)
-		// wait until all workers exit
-		log.Debug("Waiting for workers to exit")
-		wg.Wait()
-		CloseDestination()
-		CloseConfigDB()
+	SetStatus(StatusStarting)
+	err := SetupDestination()
+	if err != nil {
+		log.Error("Error setting up destination", "err", err)
+		os.Exit(1)
 	}
+	ReadMap()
+	dbmap.CompileRegexes()
+	// Create root context allowing cancellation of all goroutines
+	rootContext, rootCancel := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
+
+	// Loop through config and replicate databases
+	log.Info("Start processing source databases")
+	for _, database := range dbmap {
+		for i, url := range database.Urls {
+			log.Info("Starting replication thread", "db-sid", database.Name+"-"+url.SID, "url", url.URL)
+			wg.Add(1)
+			go DoReplicateDatabase(rootContext, database, &database.Urls[i])
+		}
+	}
+	SetStatus(StatusActive)
+
+	select {
+	case <-RootChannel:
+		rootCancel()
+	case <-rootContext.Done():
+	}
+	SetStatus(StatusStopping)
+	// wait until all workers exit
+	log.Debug("Waiting for workers to exit")
+	wg.Wait()
+	CloseDestination()
+	CloseConfigDB()
+	log.Debug("Exiting")
+
 }
